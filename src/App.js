6a59f8b1956b952e4a22db08544a488d80f50d78
redsmedeiros <91477264+redsmedeiros@users.noreply.github.com>
2022-10-17 14:59:30 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>Táticas</header>
+    </BrowserRouter>
+  );
+}
